7b890a28a4bf48b3e806f4b78d8b0f01f3649950
srikanth-vanam <B172320-@rgukt.ac.in>
2023-09-05 22:52:52 +0530

    added download-expenses(csv format) feature

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,8 @@ const Home = () => {
   const dispatch = useDispatch();
   const isActivateTheme = useSelector((state) => state.theme.isActivateTheme);
   const isDark = useSelector((state) => state.theme.isDark);
+  const expenseItems = useSelector((state) => state.expense.expenseItems);
+
   const themeToggler = () => {
     dispatch(themeActions.themeToggler());
   };
@@ -51,9 +53,18 @@ const Home = () => {
     history.replace("/");
   };
 
+  const downloadHandler = () => {
+    function makeCSV(data) {
+      return data.map((obj) => JSON.stringify(obj)).join("\n");
+    }
+    const aTag = document.getElementById("a");
+    const blob = new Blob([makeCSV(expenseItems)]);
+    aTag.href = URL.createObjectURL(blob);
+  };
+
   return (
-    <div className={`${isDark?classes.bodyDark:classes.bodyLight}`}>
-      <div className={`${classes.outer} ${isDark?classes.dark:''}`}>
+    <div className={`${isDark ? classes.bodyDark : classes.bodyLight}`}>
+      <div className={`${classes.outer} ${isDark ? classes.dark : ""}`}>
         <p>Welcome Home</p>
         <Button variant="secondary" onClick={emailVerifyHandler}>
           Verify Email
@@ -88,12 +99,15 @@ const Home = () => {
           >
             Activate Premium
           </Button>
-          <Button
-            variant="secondary"
-            className="m-3"
-            type="button"
-          >
-            Download Expenses
+          <Button variant="secondary" className="m-3" type="button">
+            <a
+              id="a"
+              download="expenses.csv"
+              onClick={downloadHandler}
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Download Expenses
+            </a>
           </Button>
         </div>
       )}
